4cec5daaaa67e7999f0ea9efa28bd02e644b8fa3
Luan <lluantenorio7@gmail.com>
2024-03-17 21:13:01 -0300

    Add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import SwaggerUi from 'swagger-ui-express';
+import SwaggerDocs from '../swagger.json'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.use('/api', SwaggerUi.serve, SwaggerUi.setup(SwaggerDocs));
 
   await app.listen(3000);
 }
